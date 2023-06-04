fc47f59b964f9253a95d2538bf150da55c48bafc
Пес <96211333+dmitriypy@users.noreply.github.com>
2023-06-04 12:18:07 +0200

    Update rates.py

--- src/handlers/text/rates.py
+++ src/handlers/text/rates.py
@@ -7,9 +7,9 @@ async def rates(message: types.Message):
 
     if len(text) > 1:
         if len(text) > 2:
-            tmp = await save_rates_by_ticker(text[1], text[2])
+            tmp = await save_rates_by_pair(text[1], text[2])
         else:
-            tmp = await save_rates_by_ticker(text[1])
+            tmp = await save_rates_by_pair(text[1])
 
         if tmp is False:
             await message.reply("Pair or Timeframe is Invalid! Try again.")
